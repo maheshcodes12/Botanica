6beab3fbc896e57628a62bc2e9faa2f3f4f8fc7c
Mahesh Suryawanshi <140623030+maheshcodes12@users.noreply.github.com>
2024-03-11 02:06:21 +0530

    Setup

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState, useEffect } from "react";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+	const [count, setCount] = useState(0);
+
+	return <>aa</>;
+}
+
+export default App;
